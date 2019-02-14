01c850d5574f3b9e7b640688bc124cce31c2f68b
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-02-14 10:15:19 +0700

    polybar: add bar for second monitor
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,10 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar
 HOST=$(cat /etc/hostname)
 if [[ ${HOST} = "archBox" ]]; then
+	SECOND_MONITOR=$(xrandr | grep VGA-1)
+	if [[ -n ${SECOND_MONITOR} ]]; then
+		polybar -r i3_second &
+	fi
 	polybar -r i3 &
 else
 	polybar -r i3-a46c &
